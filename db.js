198cbced9b1cef215e717b8ebe8bc5271924703f
Fadilix <iamfadile2@gmail.com>
2023-10-29 18:34:01 +0000

    vercel hosting

--- db.js
+++ db.js
@@ -1,12 +1,9 @@
 const { Pool } = require("pg")
 require("dotenv").config();
 const { env } = process
+
 const pool = new Pool({
-    user: env.USER,
-    host: env.HOST,
-    database: env.DATABASE,
-    password: env.PASSWORD,
-    port: env.DB_PORT,
-})
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  })
 
 module.exports = pool;
